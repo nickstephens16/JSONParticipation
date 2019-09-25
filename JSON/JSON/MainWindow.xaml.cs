98642d66577c74e67ca6dd5f4a53e95397bffa15
Nick Stephens <Nicholas.J.Stephens-1@ou.edu>
2019-09-25 11:33:46 -0500

    season 7 & 8 trash

---
+++ JSON/JSON/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            //https://got-quotes.herokuapp.com/quotes
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result; //use @ with URLs to tell it to use backslash as backslash character
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    gotQuote gq = JsonConvert.DeserializeObject<gotQuote>(content);
+
+                    txtQuote.Text = gq.quote + "\n" + " - " + gq.character;
+
+                    //var x = JsonConvert.SerializeObject(gq); //dont need this line
+                }
+            }
+
+
+        }
+    }
+}
---
+++ JSON/JSON/gotQuote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    class gotQuote
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+
+    }
+}
